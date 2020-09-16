dff2e0fe93b0ca6ee038cfcf6326a22d72b49f19
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-16 21:11:16 +0300

    asda

--- javascirpt/app.js
+++ javascirpt/app.js
@@ -9,7 +9,7 @@ else {
 if ("0") {
     alert( 'Привет' ); // true
 }*/
-let age = prompt('Введите число')
+/*let age = prompt('Введите число')
 if (age > 0){
     alert("1")
 }
@@ -18,14 +18,36 @@ else if(age <0){
 }
 else{
     alert(0)
-}
+}*/
 /*
 let result;
 let result = (a + b < 4) ?'Мало':'Много';
 alert(result);
 */
-let login = prompt('Введите логин');
+/*let login = prompt('Введите логин');
 let message = (login == 'Сотрудник') ? 'Привет':
     (login == 'Директор') ? 'Здравствуйте':
         (login == '') ? 'Нет логина' : '';
-alert( message);
+alert( message);*/
+/*const age = prompt("Год вашего рождения")
+let result =(2020 - age);
+alert(result);*/
+
+/*
+let a = prompt("Укажите объем флешки в ГБ");
+let message ="Количество файлов";
+alert((message+Math.floor((a*1024)/820)));*/
+const dollar = 74.97;
+const rub =0.013;
+let nameValue = prompt("Название валюты для обмена");
+prompt("На какую валюту")
+if (nameValue === "рубли"){
+    prompt("сумма которую хотите обменять")
+}
+ else if (nameValue === "доллар"){
+    prompt("сумма которую хотите обменять")
+}
+
+
+
+
