f3ef750682f0e29421218610bf929911c1bcb2de
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-30 21:05:45 +0300

    dz

---
+++ javascirpt/dz.js
@@ -0,0 +1,35 @@
+//10)
+/*
+alert('Задайте число 0 до 100');
+let a = 0;
+let b = 100;
+let answer = '';
+let  temp = 0;
+do{
+    temp = parseInt(a+b/2);
+    answer = prompt('Ваше число >, < или = ' + temp);
+    if(answer == '>'){
+    }else  if (answer =='<') {
+        b = temp;
+    }
+
+    }while (answer !='=');
+alert('Введите число' + temp);
+*/
+
+//9)
+/*
+for (let i = 2;i<=9;i++){
+    for (let j=1; j<=10;j++){
+        document.write((i + ' * ' + j + ' = ' + i * j + '<br>'))
+    }
+    document.write('<hr>')
+}
+*/
+//7)
+
+
+
+
+
+
