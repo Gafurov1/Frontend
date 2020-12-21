fb1ffb0d635d84b019d1977999201179bfcd5573
Студент компьютерной Академии <voronamu@gmail.com>
2020-12-21 21:14:07 +0300

    php

---
+++ php/ex1.php
@@ -0,0 +1,16 @@
+<?php
+$arr=[];
+$path= $_SERVER['DOCUMENT_ROOT'].'/img/';
+/*set_include_path(".;D:\openserver\OpenServer\domains\localhost\img");*/
+
+//1
+/*$arr=[];
+for ($i=0; $i<11; $i ++) {
+    $arr[] =rand(10,100);
+}
+print_r($arr);
+$reverse=array_reverse($arr);
+print_r($reverse);*/
+?>
+
+
